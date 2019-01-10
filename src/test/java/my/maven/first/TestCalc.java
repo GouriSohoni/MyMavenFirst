207e88daea1b7aaeba49143d8e7b7d5ba278023c
Gouri <Gouri@JenkinsVM.z1dh34imhxgujfnxzsieuttwuh.hx.internal.cloudapp.net>
2019-01-10 10:58:30 +0530

    maven project with jsp and 1 junit test

---
+++ src/test/java/my/maven/first/TestCalc.java
@@ -0,0 +1,14 @@
+package my.maven.first;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCalc {
+
+	@Test
+	public void testSum() {
+		assertEquals(true,true);
+	}
+
+}
